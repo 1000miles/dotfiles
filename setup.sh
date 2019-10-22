5086384e4ada2c35bfeaad4d9130a3532e634021
Thuy Le <gitnap@mailbox.org>
2019-10-22 16:21:09 +0200

    Update: Add vim to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-if [ -f ~/.zshrc ] ; then
-  echo "Found existing .zshrc. Moving to ~/.zshrc.old"
-  mv ~/.zshrc ~/.zshrc.old
-fi
-ln -s ~/dotfiles/.zshrc ~/.zshrc
-
+# Git
 if [ -f ~/.gitconfig ] ; then
   echo "Found existing ~/.gitconfig. Moving to ~/.gitconfig.old"
   mv ~/.gitconfig ~/.gitconfig.old
 fi
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+
+# Vim
+if [ -f ~/.vimrc ] ; then
+  echo "Found existing ~/.vimrc. Moving to ~/.vimrc.old"
+  mv ~/.vimrc ~/.vimrc.old
+fi
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+
+# Zsh
+if [ -f ~/.zshrc ] ; then
+  echo "Found existing .zshrc. Moving to ~/.zshrc.old"
+  mv ~/.zshrc ~/.zshrc.old
+fi
+ln -s ~/dotfiles/.zshrc ~/.zshrc
